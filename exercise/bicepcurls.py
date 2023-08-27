38f8eddd1ae80deef136563290c95c49ce529703
Rhythm1821 <alamrawat1821@gmail.com>
2023-08-27 22:50:22 +0530

    exercise folder

---
+++ exercise/bicepcurls.py
@@ -0,0 +1,80 @@
+from utils import calculate_angle, mp_drawing,mp_pose
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+# curl counter variables
+counter = 0
+stage = None
+
+# Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret,frame = cap.read()
+
+        # Recolor image
+        image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        image.flags.writeable=False
+
+        # Make detection
+        results = pose.process(image)
+        
+        # Recolor back to BGR
+        image.flags.writeable=True
+        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+            # Get coordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+            # Calculate angle
+            angle = calculate_angle(shoulder,elbow,wrist)
+
+            # Visualize
+            cv2.putText(image,str(angle),
+                        tuple(np.multiply(elbow,[640,480]).astype(int)),
+                              cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2,cv2.LINE_AA)
+
+            # Count curls
+            if angle > 160:
+                stage = "down"
+            if angle < 30 and stage=="down":
+                stage="Up"
+                counter+=1
+        except Exception as e:
+            print(e)
+
+        # Render curl counter
+        # setup status boxes
+        cv2.rectangle(image,(0,0),(225,73),(245,117,16),-1)
+
+        # Rep data
+        cv2.putText(image,'REPS',(15,12),
+                    cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1,cv2.LINE_AA)
+        cv2.putText(image,str(counter),(10,60),
+                    cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255),2,cv2.LINE_AA)
+        
+        # Stage data
+        cv2.putText(image,'STAGE',(65,12),
+                    cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1,cv2.LINE_AA)
+        cv2.putText(image,stage,(90,60),
+                    cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255),2,cv2.LINE_AA)
+
+        # Render detections
+        mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245,117,66),thickness=2,circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245,66,230),thickness=2,circle_radius=2))
+
+        cv2.imshow('Webcam',image)
+
+        if cv2.waitKey(10) & 0xFF==ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
