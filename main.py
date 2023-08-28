289060c2a39a3e66586b71e920f84261393c9a24
Rhythm Rawat <106919276+Rhythm1821@users.noreply.github.com>
2023-08-28 17:56:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask, redirect,render_template
+if __name__ == '__main__':
+    from exercise import bicepcurls
+    # from exercise import squats
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello flask"
 
-if __name__=="__main__":
-    app.run(host="127.0.0.1",port=5000,debug=True)
+
+
+
+
+
+
+
+# from flask import Flask, redirect,render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return "Hello flask"
+
+# if __name__=="__main__":
+#     app.run(host="127.0.0.1",port=5000,debug=True)
